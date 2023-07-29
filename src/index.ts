fa5f852ce293453edddc564f003f683533db3d64
maddsua <maddsuadev@proton.me>
2023-07-30 02:22:02 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@
 */
 
 import { parseUnifiedParams } from "./parsers.js";
-import type { RevealItemParams, Sequence } from "./types.js";
+import type { RevealItemParams, RevealContainer, RevealItem } from "./types.js";
 
 const sleep = async (timeout: number) => new Promise(resolve => setTimeout(resolve, timeout));
 
@@ -36,81 +36,77 @@ export const revealScript = (container?: HTMLElement) => {
 		return false;
 	}
 
-	const revealItems = Array.from((container || document).querySelectorAll<HTMLElement>('[data-rvl]')).map(item => ({
-		element: item as HTMLElement,
-		params: parseUnifiedParams(item.getAttribute('data-rvl')),
-	}));
-
-	const containers = revealItems.filter(item => typeof item.params.threshold === 'number').map(item => Object.assign(item, {
-		items: revealItems.filter(item1 => item.element.contains(item1.element))
-	}));
-
-	console.log(containers);
-
-	const hideElement = (element: HTMLElement, params: RevealItemParams) => {
-		
-		const dir = params.translate.direction.slice(-1);
-		const sign = params.translate.direction.length > 1 ? '-' : '';
-
-		element.style.transform = `translate${dir}(${sign}${params.translate.amountEm}em)`;
-		element.style.opacity = '0';
-		
-		if (params.transitionDelay) setTimeout(() => {
-			element.style.transition = `all ${params.transitionDelay}ms ease`;
-		}, 50);
-	};
-
-	containers.forEach(item => {
-		if (!item.items.length)
-			hideElement(item.element, item.params);
-		item.items?.forEach(item1 => hideElement(item1.element, item1.params));
-	});
-
-
-
-
-/*	
-
-	const revealSequence = async (sequence: Sequence) => {
-
-		await sleep(sequence.params.blockDelay);
-
-		sequence.items.forEach(async (item, index) => {
-
-			const itemDelay = item.params.delay || sequence.params.itemDelay;
-			const itemOrder = item.params.order < 2 ? index : item.params.order;
-
-			await sleep(itemOrder * itemDelay);
-			item.elem.style.transform = null;
-			item.elem.style.opacity = null;
-
-			await sleep(item.params.transitionDelay || 250);
-			item.elem.style.transition = null;
-		});
-	};
-	
-	const sequenceMap = new WeakMap(sequences.map(item => ([item.container, item])));
-
-	const io = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const sequence = sequenceMap.get(entry.target as HTMLElement) as Sequence;
-			if (entry.intersectionRatio < sequence.params.trigger) return;
-			revealSequence(sequence);
-			io.unobserve(entry.target);
-		});
-	}, { threshold: (Array.apply(0, Array(21)) as any[]).map((_item, index) => index * 0.05) });
-
-	sequences.forEach(sequence => io.observe(sequence.container));
-
 	const styleDirectives = [
-		'html, body {overflow-x: hidden}',
-		'[data-rvl] { transition: all 250ms ease }'
+		'html, body {overflow-x: hidden}'
 	];
 
 	const style = document.createElement('style');
 		style.id = 'reveral-v2-critical-styles';
 		style.innerHTML = styleDirectives.join('\n');
-	setTimeout(() => document.head.appendChild(style), 50);*/
+	setTimeout(() => document.head.appendChild(style), 50);
+
+	const revealItems = Array.from((container || document).querySelectorAll<HTMLElement>('[data-rvl]')).map(item => ({
+		elem: item as HTMLElement,
+		params: parseUnifiedParams(item.getAttribute('data-rvl')),
+	}));
+
+	const containers = revealItems.filter(item => typeof item.params.threshold === 'number').map(item => Object.assign(item, {
+		items: revealItems.filter(item1 => item.elem.contains(item1.elem)).filter(item1 => item.elem != item1.elem)
+	})) as RevealContainer[];
+
+	console.log(containers);
+
+	const hideElement = (elem: HTMLElement, params: RevealItemParams) => {
+		
+		const dir = params.translate.direction.slice(-1);
+		const sign = params.translate.direction.length > 1 ? '-' : '';
+
+		elem.style.transform = `translate${dir}(${sign}${params.translate.amountEm}em)`;
+		elem.style.opacity = '0';
+		
+		if (params.delay) setTimeout(() => {
+			elem.style.transition = `all ${params.delay}ms ease`;
+		}, 50);
+	};
+
+	const showElement = async (elem: HTMLElement, params: RevealItemParams) => {
+		elem.style.transform = null;
+		elem.style.opacity = null;
+		await sleep(params.delay);
+		elem.style.transition = null;
+	};
+
+	containers.forEach(item => {
+		if (!item.items.length)
+			hideElement(item.elem, item.params);
+		item.items?.forEach(item1 => hideElement(item1.elem, item1.params));
+	});
+
+	const revealSequence = async (sequence: RevealContainer) => {
+
+		await sleep(sequence.params.delay);
+
+		sequence.items.forEach(async (item, index) => {
+			const itemDelay = item.params.delay || sequence.params.childDelay;
+			const itemOrder = item.params.index < 2 ? index : item.params.index;			
+			await sleep(itemOrder * itemDelay);
+			await showElement(item.elem, item.params);
+		});
+	};
+
+	const sequenceMap = new WeakMap(containers.map(item => ([item.elem, item])));
+
+	const io = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const sequence = sequenceMap.get(entry.target as HTMLElement);
+			if (entry.intersectionRatio < sequence.params.threshold) return;
+			sequence.items.length ? revealSequence(sequence) : showElement(sequence.elem, sequence.params);
+			io.unobserve(entry.target);
+		});
+	}, { threshold: (Array.apply(0, Array(21)) as any[]).map((_item, index) => index * 0.05) });
+
+	containers.forEach(sequence => io.observe(sequence.elem));
+
 };
 
 export default revealScript;
