d3d43bf194fd738a4838d0319772515b6a668874
maddsua <maddsuadev@proton.me>
2023-07-30 02:21:58 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -5,32 +5,26 @@ export interface Translate {
 	amountEm: number;
 };
 
-export interface RevealItem {
-	elem: HTMLElement;
-	params: {
-		translate: number;
-		direction: Direction;
-		order: number;
-		delay: number | null;
-		transitionDelay?: number;
-	};
-}
-
-export interface Sequence {
-	container: HTMLElement;
-	params: {
-		trigger: number;
-		blockDelay: number;
-		itemDelay: number;
-	};
-	items: RevealItem[];
-}
-
-export interface RevealItemParams {
-	threshold: number | undefined;	//	t25
-	childDelay: number;				//	cd250
+export type RevealItemParams = {
 	delay: number;					//	d125
 	translate: Translate;			//	tl2
-	transitionDelay: number;		//	td250
 	index: number;					//	i5
 };
+
+export interface RevealItem {
+	elem: HTMLElement;
+	params: RevealItemParams;
+}
+
+export type RevealContainerParams = {
+	threshold: number | undefined;	//	t25
+	childDelay: number;				//	cd250
+	translate: Translate;			//	tl2
+	index: number;					//	i5
+} & RevealItemParams;
+
+export interface RevealContainer {
+	elem: HTMLElement;
+	items: RevealItem[];
+	params: RevealContainerParams;
+}
