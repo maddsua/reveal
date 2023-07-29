d78f33b87b4581d9b36c4835dd2763eb14e73fb4
maddsua <maddsuadev@proton.me>
2023-07-30 01:25:04 +0300

    Update parsers.ts

--- src/parsers.ts
+++ src/parsers.ts
@@ -2,40 +2,42 @@ import type { Direction, RevealItemParams } from "./types.js";
 
 export const parseUnifiedParams = (attribText: string | null): RevealItemParams => {
 
-	const directives = attribText?.toLowerCase();
+	const directives = attribText?.toLowerCase()?.split(' ');
+
+	const getArg = (expr: RegExp) => directives.find(item => expr.test(item));
 
 	return {
 		threshold: (() => {
-			const arg = directives?.match(/f\d+/)?.[0]?.replace(/\D/g, '');
+			const arg = getArg(/^t\d+$/)?.slice(1);
 			return (arg ? parseInt(arg) : 25) / 100;
 		})(),
 		delay: (() => {
-			const arg = directives?.match(/b\d+/)?.[0]?.replace(/\D/g, '');
+			const arg = getArg(/^d\d+$/)?.slice(1);
 			return arg ? parseInt(arg) : 350;
 		})(),
 		childDelay: (() => {
-			const arg = directives?.match(/i\d+/)?.[0]?.replace(/\D/g, '');
+			const arg = getArg(/^cd\d+$/)?.slice(2);
 			return arg ? parseInt(arg) : 50;
 		})(),
 		translate: (() => {
-			const arg = directives?.match(/t\d+/)?.[0]?.replace(/\D/g, '');
-			return arg ? parseInt(arg) : 2;
-		})(),
-		direction: (() => {
-			const arg = directives?.match(/d[rlbt]/)?.[0];
-			return arg ? {
-				't': '-y',
-				'b': 'y',
-				'l': '-x',
-				'r': 'x'
-			}[arg[1]] as Direction : 'y';
+			const arg = getArg(/^t[rltb]\d*$/);
+			const compNumeric = arg?.slice(2);
+			return {
+				amountEm: compNumeric ? parseInt(compNumeric) : 2,
+				direction: {
+					't': '-y',
+					'b': 'y',
+					'l': '-x',
+					'r': 'x'
+				}[arg[2]] as Direction || 'y'
+			};
 		})(),
 		transitionDelay: (() => {
-			const arg = directives?.match(/tt\d+/)?.[0]?.replace(/\D/g, '');
+			const arg = getArg(/^td\d+*$/)?.slice(2);
 			return arg ? parseInt(arg) : undefined;
 		})(),
 		index: (() => {
-			const arg = directives?.match(/o\d+/)?.[0]?.replace(/\D/g, '');
+			const arg = getArg(/^i\d+*$/)?.slice(1);
 			return arg ? parseInt(arg) : 1;
 		})()
 	}
