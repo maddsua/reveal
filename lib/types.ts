8038276c56bd1ee42edc00cc506b2b3bfbfcdf95
maddsua <maddsuadev@proton.me>
2023-09-07 21:40:35 +0300

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,14 @@
+export type Direction = '-x' | 'x' | '-y' | 'y';
+
+export interface Translate {
+	direction: Direction;
+	amountEm: number;
+};
+
+export type RevealParams = {
+	threshold: number;				//	t25 - reveal threshold in persents
+	delay: number;					//	d125 - animation delay
+	length: number;					//	l250 - animation length
+	translate: Translate;			//	tl2 - transform
+	index: number;					//	i5 - reveal order
+};
