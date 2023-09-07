df2ef25e11b60d1a9ff458f255e82aadb2feca36
maddsua <maddsuadev@proton.me>
2023-09-07 21:46:33 +0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -6,11 +6,11 @@ export interface Translate {
 };
 
 export interface RevealParams {
-	threshold: number;				//	t25 - reveal threshold in persents
-	delay: number;					//	d125 - animation delay
-	length: number;					//	l250 - animation length
-	translate: Translate;			//	tl2 - transform
-	index: number;					//	i5 - reveal order
+	threshold: number;			//	t25 - reveal threshold in persents
+	delay: number;				//	d125 - animation delay
+	length: number;				//	l250 - animation length
+	translate: Translate;		//	tl2 - transform
+	index: number;				//	i5 - reveal order
 };
 
 export interface RevealItem {
