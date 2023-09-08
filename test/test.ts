fceffb36eb00e3315770de59898eb287b6653672
maddsua <maddsuadev@proton.me>
2023-09-08 23:36:15 +0300

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,6 @@
 import attributeParser from '../lib/attributeParser';
 
-const attribute = 't1 tr1 d1 l1 i1 ct1 ctr1 cd1 cl1 ci1';
+const attribute = 't1 tr1 d1 l1 i1 ct1 ctr1 cd1 cl1';
 
 const result = attributeParser(attribute)
 
