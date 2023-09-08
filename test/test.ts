9b91cfcba01af28263366c2f126863062dae3a53
maddsua <maddsuadev@proton.me>
2023-09-08 23:32:05 +0300

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -2,4 +2,6 @@ import attributeParser from '../lib/attributeParser';
 
 const attribute = 'ct18';
 
-console.log(attributeParser(attribute));
+const result = attributeParser(attribute)
+
+console.log(result);
